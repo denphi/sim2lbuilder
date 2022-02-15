c56a042a2cbd0b18403fedb0dd2141d5688cc68f
Denphi <denphi@denphi.com>
2022-02-15 16:24:48 -0500

    code builder

--- sim2lbuilder.py
+++ sim2lbuilder.py
@@ -2,24 +2,75 @@ import warnings
 import importlib
 import simtool
 from IPython.display import display
-from traitlets import Dict, validate, Unicode, List
-from ipywidgets import HBox, Label, Output
+from traitlets import Dict, validate, Unicode, List, Instance
+from ipywidgets import VBox, HBox, Label, Output, FileUpload, HTML
 import ipysheet
+from IPython.display import FileLink
+from PIL import Image
+import io, json
+import inspect
 
-class WidgetConstructor():
-    def __init__(self, layx=None):
+
+class WidgetConstructor(VBox):
+    def __init__(self, layx=None, **kwargs):
         self.inputs = {}
         self.outputs = {}
-        self.layout = None
+        self.libraries = {"ipywidgets":[]}
+        self.containers = {}
+        self.clicks = []
+        self.methods = []
+        self.format = kwargs.get("format","object")
+        self.widget_name = kwargs.get("widget_name","MyWidget")
         for item, value in layx.items():
             if item == "inputs":
                 self.buildParameters(value, self.inputs)  
             elif item == "outputs":
                 self.buildParameters(value, self.outputs)  
             elif item == "layout":
-                self.layout = self.buildLayout(value)  
+                self._layout = self.buildLayout(value)  
             else:
-               pass;#warnings.warn(item + " is ignored")  
+               pass;#warnings.warn(item + " is ignored") 
+        VBox.__init__(self, **kwargs);
+            
+    def assemble(self):
+        if self.format=="object":
+            self.children = [self._layout]
+            
+        elif self.format=="text" or self.format=="file":
+            textobject = ""
+            for v in self.libraries.keys():
+                textobject += "import " + str(v) + "\n"
+            textobject += "class " + self.widget_name + "(ipywidgets.VBox):\n"
+            textobject += "  def __init__(self, **kwargs):\n"
+            textobject += "    self.inputs = {};\n"
+            textobject += "    self.outputs = {};\n"
+            textobject += "    self.containers = {};\n"
+            for k,v in self.containers.items():
+                textobject += "    self.containers['"+k+"'] = "+v+";\n"
+            for k,v in self.inputs.items():
+                textobject += "    self.inputs['"+k+"'] = "+v+";\n"
+            for k,v in self.outputs.items():
+                textobject += "    self.outputs['"+k+"'] = "+v+";\n"
+            for v in self.clicks:
+                textobject += "    "+v+";\n"
+            textobject += "    ipywidgets.VBox.__init__(self, **kwargs);\n"
+            textobject += "    self.children = [" + self._layout+"];\n"
+            for m in self.methods:
+                try:
+                    lines = inspect.getsource(getattr(self, m))
+                    for l in lines.split('\n'):
+                        textobject += "  " + l + "\n"
+                except:
+                    pass;
+            if self.format=="text":
+                self.children = [HTML(value="<pre>" + textobject+"</pre>")]
+            else:
+                f = open(self.widget_name + ".py", "w")
+                f.write(textobject)
+                f.close()
+                self.children = [HTML(value="<a href='"+ self.widget_name + ".py"+"' target='blank'>Download File</a>")]
+        else:
+            raise Except("Format not valid")
             
     def buildParameters(self, outputs, storage):
         for item, value in outputs.items():
@@ -40,11 +91,32 @@ class WidgetConstructor():
                     elif item2 == "module":
                         module_ = value2;
                     else:
-                        params_[item2] = value2
-                module = importlib.import_module(module_)
-                class_ = getattr(module, type_)
-                storage[item] = class_(**params_)   
-                self.setClick(storage[item], click_)                
+                        try:
+                            value2j = json.dumps(value2)
+                            if value2j != "null":
+                                params_[item2] = value2
+                        except:
+                            pass;
+                uuid = "cont" + str(len(self.containers.keys()))        
+                if self.format=="object":
+                    module = importlib.import_module(module_)
+                    class_ = getattr(module, type_)
+                    self.containers[uuid] = class_(**params_) 
+                    storage[item] = self.containers[uuid]
+                    self.setClick(storage[item], click_)                
+                
+                elif self.format=="text" or self.format=="file":
+                    if module_ not in self.libraries:
+                        self.libraries[module_] = {}
+                    self.containers[uuid] = module_+"."+type_+"(" + ",".join([str(k)+"="+json.dumps(v) for k,v in params_.items()])+ ")"
+                    storage[item] = "self.containers['" + uuid+ "']"
+
+                    if click_ is not None:
+                        self.methods.append(click_) 
+                        self.clicks.append("self.containers['" + uuid+ "'].on_click(lambda a, b=self, c='" + click_ + "' : getattr(b, c)(b))")                
+
+                else:
+                    raise Except("Format not valid")
             else:
                warnings.warn(item + " is not a valid description")  
 
@@ -70,7 +142,11 @@ class WidgetConstructor():
                     for item2, value2 in value.items():
                         child_ = None
                         if value2 == None:
-                            if (item2 in self.inputs):
+                            if (item2.replace("input.","") in self.inputs):
+                                child_ = self.inputs[item2.replace("input.","")]
+                            elif (item2.replace("output.","") in self.outputs):
+                                child_ = self.outputs[item2.replace("output.","")]
+                            elif (item2 in self.inputs):
                                 child_ = self.inputs[item2]
                             elif (item2 in self.outputs):
                                 child_ = self.outputs[item2]
@@ -84,35 +160,51 @@ class WidgetConstructor():
                    warnings.warn(item + " is not a valid description")
 
             else:
-                params_[item] = value
-        if (len(titles_) > 0 ):
-            params_["titles"] = titles_
-        if (len(children_) > 0 ):
-            params_["children"] = children_
-        module = importlib.import_module(module_)
-        class_ = getattr(module, type_)
-        instance_ =  class_(**params_) 
-        self.setClick(instance_, click_)
-        
-        for i, title in enumerate(titles_):
-            try:
-                instance_.set_title(i, title)
-            except:
-               warnings.warn(title + " can not be assigned")
-
+                if self.format=="text" or self.format=="file":
+                    params_[item] = json.dumps(value)
+                else:
+                    params_[item] = value
+            
+        uuid = "cont" + str(len(self.containers.keys()))
+        if self.format=="object":        
+            if (len(titles_) > 0 ):
+                params_["titles"] = titles_
+            if (len(children_) > 0 ):
+                params_["children"] = children_
+            module = importlib.import_module(module_)
+            class_ = getattr(module, type_)
+            self.containers[uuid] = class_(**params_) 
+            instance_ = self.containers[uuid]
+            self.setClick(instance_, click_)
+            for i, title in enumerate(titles_):
+                try:
+                    instance_.set_title(i, title)
+                except:
+                   warnings.warn(title + " can not be assigned")
+                
+        elif self.format=="text" or self.format=="file":
+            if (len(titles_) > 0 ):
+                params_["titles"] =  "['" + "','".join(titles_) + "']"
+            if (len(children_) > 0 ):
+                params_["children"] = "[" + ",".join(children_) + "]"
+            if module_ not in self.libraries:
+                self.libraries[module_] = {}
+            self.containers[uuid] = module_+"."+type_+"(" + ",".join([str(k)+"="+str(v) for k,v in params_.items()])+ ")"
+            if click_ is not None:
+                self.methods.append(click_) 
+                self.clicks.append("self.containers['" + uuid+ "'].on_click(lambda a, b=self, c='" + click_ + "' : getattr(b, c)(b))")                
+            instance_ = "self.containers['" + uuid+ "']"
+        else:
+            raise Except("Format not valid")
         return instance_
          
     def setClick(self, instance, function_name):
         if function_name is not None:
             instance.on_click(None, remove=True)
-            instance.on_click(lambda a, b=self, c=function_name: getattr(b, c)(b))
-               
-        
-    def display(self):
-        display(self.layout)
+            instance.on_click(lambda a, b=self, c=function_name: getattr(b, c)(b))   
         
         
-def GetSimtoolDefaultSchema( simtool_name ):
+def GetSimtoolDefaultSchema( simtool_name, **kwargs ):
     schema = simtool_constructor(None, type('Node', (object,), {"value" :simtool_name}))
     return {
         'inputs': schema['inputs'],
@@ -125,20 +217,23 @@ def GetSimtoolDefaultSchema( simtool_name ):
                         'width' : 'auto'
                     },
                     'type': 'VBox',
-                    'children' : {c:None for c in schema['inputs'].keys()}
+                    'children' : {"input."+str(c):None for c in schema['inputs'].keys()}
                 },
                 'outputs' : {
                     'type': 'VBox',
                     'children':{ 
                         'button':{
                             'type' : 'Button',
-                            'click' : 'RunSimTool',
-                            'description' : 'Run SimTool'
+                            'click' : kwargs.get('button_click','RunSimTool'),
+                            'description' : kwargs.get('button_description','Run SimTool')
                         }, 'container' : {
-                            'type': 'Tab',
-                            'children' : {c:None for c in schema['outputs'].keys()} ,
-                            'titles': {c:None for c in schema['outputs'].keys()}  
+                            'type': kwargs.get('outputs_layout','Accordion'),
+                            'children' : {"output." + str(c):None for c in schema['outputs'].keys()} ,
+                            'titles': {c:None for c in schema['outputs'].keys()}
                         }
+                    },
+                    'layout':{
+                        'flex':'1'
                     }
                 }
             }
@@ -157,36 +252,44 @@ def simtool_constructor(self, node):
         action = values[2]        
     stl = simtool.searchForSimTool(tool)
     if (stl['notebookPath'] == None):
-        raise "Simtool is not valid"
+        raise Except("Simtool is not valid")
     inputs = simtool.getSimToolInputs(stl)
     outputs = simtool.getSimToolOutputs(stl)
     res = {'inputs':{},'outputs':{}}
     for i in inputs:
-        res['inputs'][i] = {}
-        for j in inputs[i]:
-            if inputs[i][j] is None:
-                pass;
-            elif j == "type":
-                if inputs[i][j] == "Integer":
-                    res['inputs'][i][j] = "BoundedIntText"
-                elif inputs[i][j] == "Number":
-                    res['inputs'][i][j] = "BoundedFloatText"
-                elif inputs[i][j] == "Boolean":
-                    res['inputs'][i][j] = "ToggleButton"
-                elif inputs[i][j] == "List":
-                    res['inputs'][i][j] = "ListSheet"
-                    res['inputs'][i]["module"] = "sim2lbuilder"
-                elif inputs[i][j] == "Dict":
-                    res['inputs'][i][j] = "DictSheet"
-                    res['inputs'][i]["module"] = "sim2lbuilder"
-                #elif inputs[i][j] == "List":
-                #    res['inputs'][i][j] = "TagsInput"
-                else:
-                    res['inputs'][i][j] = "Text"
-            elif j == "desc":                 
-                res['inputs'][i]["description"] = inputs[i][j]
-            else :
-                res['inputs'][i][j] = inputs[i][j]
+        if inputs[i].type in [None, "Element"]:
+            pass;
+        else:
+            res['inputs'][i] = {}
+            for j in inputs[i]:
+                if inputs[i][j] is None:
+                    pass;
+                elif j == "type":
+                    if inputs[i][j] == "Integer":
+                        res['inputs'][i][j] = "BoundedIntText"
+                    elif inputs[i][j] == "Number":
+                        res['inputs'][i][j] = "BoundedFloatText"
+                    elif inputs[i][j] == "Boolean":
+                        res['inputs'][i][j] = "ToggleButton"
+                    elif inputs[i][j] == "List" or inputs[i][j] == "Array":
+                        res['inputs'][i][j] = "ListSheet"
+                        res['inputs'][i]["module"] = "sim2lbuilder"
+                    elif inputs[i][j] == "Dict":
+                        res['inputs'][i][j] = "DictSheet"
+                        res['inputs'][i]["module"] = "sim2lbuilder"
+                    elif inputs[i][j] == "Choice":
+                        res['inputs'][i][j] = "Dropdown"
+                    elif inputs[i][j] == "Image":
+                        res['inputs'][i][j] = "ImageUpload"
+                        res['inputs'][i]["module"] = "sim2lbuilder"
+                    #elif inputs[i][j] == "List":
+                    #    res['inputs'][i][j] = "TagsInput"
+                    else:
+                        res['inputs'][i][j] = "Text"
+                elif j == "desc":                 
+                    res['inputs'][i]["description"] = inputs[i][j]
+                else :
+                    res['inputs'][i][j] = inputs[i][j]
             
     for i in outputs:
         res['outputs'][i] = {}
@@ -268,7 +371,7 @@ class ListSheet(HBox):
         self._table = ipysheet.Sheet(columns = 1, row_headers = False, column_headers = ["value"])
         self._label = Label(kwargs.get("description", ""))
         self.updating = False
-        self.value = kwargs.get("value", {})
+        self.value = kwargs.get("value", [])
         kwargs["children"] = [self._label, self._table]
         HBox.__init__(self, **kwargs);
    
@@ -308,4 +411,31 @@ class ListSheet(HBox):
     def _valid_description(self, proposal):
         self._label.value = proposal['value']
         return proposal['value']
+
+
+class ImageUpload(HBox):
+    value = Instance(Image.Image).tag(sync=False)
+    description = Unicode("").tag(sync=True)
+    def __init__(self, **kwargs):
+        self.debug = Output()
+        self._upload = FileUpload(accept='image/*',multiple=False)
+        self._upload.observe(lambda c, s=self: ImageUpload._handle_change(s, c), names='_counter')
+        self._label = Label(kwargs.get("description", ""))
+        self.updating = False
+        self.value = kwargs.get("value", Image.open("nanohub.png"))
+        kwargs["children"] = [self._label, self._upload]
+        HBox.__init__(self, **kwargs);
+   
+    def _handle_change(self, change):
+        if self._upload._counter > 0 :
+            content = list(self._upload.value.values())[0]['content']
+            name = list(self._upload.value.values())[0]['metadata']['name']
+            self.value = Image.open(io.BytesIO(content))
+            self._upload._counter = 0
+        
     
+    @validate('description')
+    def _valid_description(self, proposal):
+        self._label.value = proposal['value']
+        return proposal['value']
+
